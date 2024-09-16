7ff0ae62824e4012a0087fd3a7a4e733a274d05d
Your Name <you@example.com>
2024-09-16 19:59:45 +0100

    project boostrap

---
+++ app/(roots)/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Tabs } from "expo-router";
+
+const Layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen name="home" options={{ headerShown: false }} />
+    </Tabs>
+  );
+};
+
+export default Layout;
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <SafeAreaView className='bg-red-500 h-screen'>
+      <Text>index</Text>
+    </SafeAreaView>
+  )
+}
+
+export default index
